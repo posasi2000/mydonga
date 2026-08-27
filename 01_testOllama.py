6c5616d26f100d3e70819609109b380b9887d0c3
young <posasi2000@users.noreply.github.com>
2026-08-27 12:39:08 +0900

    08-27-목요일 올라마 수정
    Updated the prompt template to personalize the AI response and removed commented-out code for alternative models.

--- 01_testOllama.py
+++ 01_testOllama.py
@@ -14,26 +14,10 @@ from langchain_core.prompts import ChatPromptTemplate
 #1 모델기술
 model = ChatOllama(model='gemma3:latest')
 
-# model = ChatOpenAI(model='gpt-4o-mini')   유료 openai
-# api_key = os.getenv('OPENROUTER_API_KEY')
-# model = ChatOpenAI(
-#   model='openai/gpt-oss-20b:free', 
-#   openai_api_key=api_key, 
-#   openai_api_base='https://openrouter.ai/api/v1'
-# )
-
-
-# 더권장
-# api_key = os.getenv('OPENROUTER_API_KEY')
-# model = ChatOpenAI(
-#     model          = "openai/gpt-4o-mini",        
-#     openai_api_key = api_key,
-#     base_url       = "https://openrouter.ai/api/v1" 
-# )
 
 #2 프롬프트 템플릿
 prompt = ChatPromptTemplate([
-   ("system", "당신은 친절하고 전문적인 인공지능 AI선생님이야. 사용자의 질문에 한국어로 친절히 답해주세요."),
+   ("system", "길동님은 친절하고 전문적인 인공지능 AI선생님이야. 사용자의 질문에 한국어로 친절히 답해주세요."),
    ("user", "{question}에 대해서 설명해줘")
   ])
 
@@ -42,3 +26,4 @@ chain = prompt | model | StrOutputParser()
 result = chain.invoke({'question':'미녀와야수'})
 print(result)
 
+# 2026-08-27일  목요일  12시 37분 
